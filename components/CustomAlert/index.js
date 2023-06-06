fda90e4d2dbe181538d04ff1bfa34ad14f774988
Tomas-Lawton <tlaw8603@uni.sydney.edu.au>
2023-06-06 14:25:31 +1200

    shadow class

--- components/CustomAlert/index.js
+++ components/CustomAlert/index.js
@@ -20,7 +20,7 @@ const CustomAlert = ({ handleContactScroll }) => {
 
   return (
     showAlert &&
-    <div onClick={handleClickAlert} className={`transition-all duration-300 ease-out hover:scale-105 text-center py-4 lg:px-4 w-100 fixed bottom-5 z-50`}>
+    <div onClick={handleClickAlert} className={`custom-drop transition-all duration-300 ease-out hover:scale-105 text-center py-4 lg:px-4 w-100 fixed bottom-5 z-50`}>
       <div className="p-3 bg-slate-100 items-center text-indigo-100 leading-none rounded-full flex lg:inline-flex" role="alert">
         <span className="flex rounded-full bg-accent text-center uppercase p-3 py-1 text-xs font-bold mr-3">Open to work</span>
         <span className="font mr-2 text-left flex-auto text-slate-600">Click here to reach me.</span>
