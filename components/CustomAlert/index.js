0374b10a63cf5133be3b42c41d9bdb933a66b589
Tomas-Lawton <tlaw8603@uni.sydney.edu.au>
2023-06-06 14:29:43 +1200

    fix mistake

--- components/CustomAlert/index.js
+++ components/CustomAlert/index.js
@@ -20,8 +20,8 @@ const CustomAlert = ({ handleContactScroll }) => {
 
   return (
     showAlert &&
-    <div onClick={handleClickAlert} className={`custom-drop transition-all duration-300 ease-out hover:scale-105 text-center py-4 lg:px-4 w-100 fixed bottom-5 z-50`}>
-      <div className="p-3 bg-slate-100 items-center text-indigo-100 leading-none rounded-full flex lg:inline-flex" role="alert">
+    <div onClick={handleClickAlert} className={`transition-all duration-300 ease-out hover:scale-105 text-center py-4 lg:px-4 w-100 fixed bottom-5 z-50`}>
+      <div className="custom-drop p-3 bg-slate-100 items-center text-indigo-100 leading-none rounded-full flex lg:inline-flex" role="alert">
         <span className="flex rounded-full bg-accent text-center uppercase p-3 py-1 text-xs font-bold mr-3">Open to work</span>
         <span className="font mr-2 text-left flex-auto text-slate-600">Click here to reach me.</span>
         <svg className="fill-current opacity-75 h-4 w-4 text-slate-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" /></svg>
